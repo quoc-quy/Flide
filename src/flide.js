ba738e7f60f02c24d6ab20672334ec597a185e5a
quoc-quy <quyleom10@gmail.com>
2025-08-17 22:17:31 +0700

    Thêm thành phần Nav cho Flide

--- src/flide.js
+++ src/flide.js
@@ -10,6 +10,7 @@ function Flide(selector, options = {}) {
             items: 1,
             loop: false,
             speed: 300,
+            nav: true,
         },
         options
     );
@@ -22,8 +23,19 @@ function Flide(selector, options = {}) {
 
 Flide.prototype._init = function () {
     this.container.classList.add("flide-wrapper");
+    this._createContent();
     this._createTrack();
-    this._createNavigation();
+    this._createControl();
+
+    if (this.opt.nav) {
+        this._createNav();
+    }
+};
+
+Flide.prototype._createContent = function () {
+    this.content = document.createElement("div");
+    this.content.className = "flide-content";
+    this.container.appendChild(this.content);
 };
 
 Flide.prototype._createTrack = function () {
@@ -46,9 +58,10 @@ Flide.prototype._createTrack = function () {
         slider.style.flexBasis = `calc(100% / ${this.opt.items})`;
         this.track.appendChild(slider);
     });
+    this.content.appendChild(this.track);
 };
 
-Flide.prototype._createNavigation = function () {
+Flide.prototype._createControl = function () {
     this.btnPrev = document.createElement("button");
     this.btnPrev.classList.add("flide-prev");
     this.btnPrev.innerHTML = "<";
@@ -57,7 +70,7 @@ Flide.prototype._createNavigation = function () {
     this.btnNext.classList.add("flide-next");
     this.btnNext.innerHTML = ">";
 
-    this.container.append(this.track, this.btnPrev, this.btnNext);
+    this.content.append(this.track, this.btnPrev, this.btnNext);
 
     this.btnPrev.onclick = () => this.moveSlide(-1);
     this.btnNext.onclick = () => this.moveSlide(1);
@@ -78,15 +91,62 @@ Flide.prototype.moveSlide = function (step) {
         if (this.opt.loop) {
             if (this.currentIndex <= 0) {
                 this.currentIndex = maxIndex - this.opt.items;
+                this._updatePosition(true);
             } else if (this.currentIndex >= maxIndex) {
                 this.currentIndex = this.opt.items;
+                this._updatePosition(true);
             }
-            this._updatePosition(true);
         }
         this._isAnimating = false;
     }, this.opt.speed);
 
     this._updatePosition();
+};
+
+Flide.prototype._createNav = function () {
+    this.navWrapper = document.createElement("div");
+    this.navWrapper.classList.add("flide-nav");
+
+    const slideCount = this.opt.loop
+        ? this.sliders.length - this.opt.items * 2
+        : this.sliders.length;
+
+    const pageCount = Math.ceil(slideCount / this.opt.items);
+
+    for (let i = 0; i < pageCount; i++) {
+        const dot = document.createElement("button");
+        dot.className = "flide-dot";
+
+        if (i === 0) dot.classList.add("active");
+
+        dot.onclick = () => {
+            this.currentIndex = this.opt.loop
+                ? i * this.opt.items + this.opt.items
+                : i * this.opt.items;
+            this._updatePosition();
+        };
+
+        this.navWrapper.appendChild(dot);
+    }
+
+    this.container.appendChild(this.navWrapper);
+};
+
+Flide.prototype._updateNav = function () {
+    let realIndex = this.currentIndex;
+
+    if (this.opt.loop) {
+        const slideCount = this.sliders.length - this.opt.items * 2;
+        realIndex =
+            (this.currentIndex - this.opt.items + slideCount) % slideCount;
+    }
+
+    const pageIndex = Math.floor(realIndex / this.opt.items);
+
+    const dots = Array.from(this.navWrapper.children);
+    dots.forEach((dot, index) => {
+        dot.classList.toggle("active", index === pageIndex);
+    });
 };
 
 Flide.prototype._updatePosition = function (instant = false) {
@@ -95,4 +155,8 @@ Flide.prototype._updatePosition = function (instant = false) {
         : `transform ease ${this.opt.speed}ms`;
     this.offset = -(this.currentIndex * (100 / this.opt.items));
     this.track.style.transform = `translateX(${this.offset}%)`;
+
+    if (this.opt.nav && !instant) {
+        this._updateNav();
+    }
 };
